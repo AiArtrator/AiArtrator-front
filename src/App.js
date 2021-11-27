834100ecc339f7ca999f4b563a8b17fbd44661fc
DODO <thsehgml37@korea.ac.kr>
2021-11-27 12:30:49 +0900

    feat: navbar상단 고정기능 추가

--- src/App.js
+++ src/App.js
@@ -7,22 +7,23 @@ import LogInPage from './page/LoginPage/Index.js';
 import NavBar from './components/NavBar/Index.js';
 import OwnNetworksPage from './page/OwnNetworksPage/Index.js';
 import MyInfoPage from './page/MyInfoPage/Index.js';
-// import GlobalStyle from './GlobalStyle';
 
 function App() {
 	return (
-		<div className="App">
+		<div>
 			<NavBar />
-			<Test />
-			<OwnNetworksPage />{' '}
-			{/*  App에서 이렇게 하면 화면에 잘 보임!!!!SCSS문제인듯 */}
-			<Routes>
-				<Route path="/" component={MainPage} exact={true} />
-				<Route path="/NetworkLists" component={NetworkListPage} />
-				<Route path="/LogIn" component={LogInPage} />
-				<Route path="/OwnNetworks" component={OwnNetworksPage} />
-				<Route path="/MyInfo" component={MyInfoPage} />
-			</Routes>
+			<div className="layout">
+				<OwnNetworksPage />
+				{/*  App에서 이렇게 하면 화면에 잘 보임!!!!SCSS문제인듯 */}
+				<Test />
+				<Routes>
+					<Route path="/" component={MainPage} exact={true} />
+					<Route path="/NetworkLists" component={NetworkListPage} />
+					<Route path="/LogIn" component={LogInPage} />
+					<Route path="/OwnNetworks" component={OwnNetworksPage} />
+					<Route path="/MyInfo" component={MyInfoPage} />
+				</Routes>
+			</div>
 		</div>
 	);
 }
--- src/components/NavBar/Index.js
+++ src/components/NavBar/Index.js
@@ -4,34 +4,36 @@ import Logo from './NavTitle/Index.js';
 import ModelLists from './ModelLists/Index.js';
 import MyMenus from './MyMenu/Index.js';
 import LogIn from './LogInButton/Index.js';
-import OwnNetworksBtn from './MyMenu/OwnNetworksButton/Index.js';
+
 import { Link } from 'react-router-dom';
 
 const Index = () => {
 	return (
-		<div className="dropmenu">
-			<ul>
-				<li>
-					<Logo />
-				</li>
-				<li>
-					<ModelLists />
-				</li>
-				<li>
-					<MyMenus />
-					<ul>
-						<li>
-							<OwnNetworksBtn />
-						</li>
-						<li>
-							<Link to="/">임시 로그아웃</Link>
-						</li>
-					</ul>
-				</li>
-				<li>
-					<LogIn />
-				</li>
-			</ul>
+		<div className="nav-layout">
+			<div className="dropmenu">
+				<ul>
+					<li>
+						<Logo />
+					</li>
+					<li>
+						<ModelLists />
+					</li>
+					<li>
+						<MyMenus />
+						<ul>
+							<li>
+								<Link to="/NetworkLists">내가 구매한 모델들</Link>
+							</li>
+							<li>
+								<Link to="/">임시 로그아웃</Link>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<LogIn />
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
