949709884aeb97443241f2668661e093e5454a79
DODO <thsehgml37@korea.ac.kr>
2021-12-29 17:49:25 +0900

    Fix: 스펠링 대문자 소문자 miss, 수정 완료

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,3 +1,3 @@
-export { default as SignupForm } from './signupForm';
+export { default as SignupForm } from './SignupForm';
 export { default as SubmitButton } from './submitContainer';
 export { default as Input } from './inputContainer';
