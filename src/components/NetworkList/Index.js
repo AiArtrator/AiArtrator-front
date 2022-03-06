ed21aaa3a4e49b5363e51f7178c743f3145a4c69
DODO <thsehgml37@korea.ac.kr>
2022-03-06 22:45:39 +0900

    Fix: 컴포넌트 이름 수정 모델리스트페이지 이름을 networklist 로

--- src/components/NetworkList/Index.js
+++ src/components/NetworkList/Index.js
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './network-list.scss';
+import NetworksItem from './NetworkItems/Index.js';
+import { getNetworkList } from '../../axios/Network';
 
-const Index = function () {
-	return <></>;
+const Index = () => {
+	const [networks, setNetworks] = useState(null);
+	const [networksCount, setNetworksCount] = useState(0);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			setError(null);
+			setNetworks(null);
+			setLoading(true);
+			try {
+				const response = await getNetworkList(null); // TODO : replace to getNetworkByUserId
+				setNetworks(response.data.postList);
+				setNetworksCount(response.data.postListCount);
+			} catch (err) {
+				console.error(err);
+				setError(err);
+			}
+			setLoading(false);
+		};
+		fetchData();
+	}, []);
+
+	// 대기중일때
+	if (loading) {
+		return <div className="list-block">로딩 중</div>;
+	}
+	if (error) return <div>에러가 발생했습니다</div>;
+	// 아직 networks값이 설정되지 않았을때
+	if (!networks) {
+		console.log('아직 networks값이 설정되지 않음');
+		return null;
+	}
+
+	console.log(networks);
+	console.log('networks.postList');
+	console.log(networks.postList);
+	console.log('networksCount');
+	console.log(networksCount);
+
+	// networks 값이 유효할때
+	return (
+		<div className="list-block">
+			{networksCount}
+			{networks.map((network) => {
+				return <NetworksItem key={network.id} network={network} />;
+			})}
+		</div>
+	);
 };
 
 export default Index;
---
+++ src/components/NetworkList/NetworkItems/Index.js
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import './network-items.scss';
+
+const Index = ({ network }) => {
+	const { thumbnail, title, summary } = network;
+
+	return (
+		<div className="items-block">
+			<div className="thumbnail">
+				<a>
+					{/* <a href={thumbnail} target="_blank" rel="noopener noreferrer" /> */}
+					<img src={thumbnail} alt="thumbnail" />
+				</a>
+			</div>
+
+			<div className="contents">
+				<h3>
+					<a>
+						{/* <a href={url} target="_blank" rel="noopener noreferrer"> */}
+						{title}
+					</a>
+				</h3>
+				<p>{summary}</p>
+			</div>
+		</div>
+	);
+};
+
+Index.propTypes = {
+	network: PropTypes.object,
+};
+
+export default Index;
