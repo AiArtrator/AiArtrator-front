d67a207afefe7fc95ed577dd5ce86addae86d289
DODO <thsehgml37@korea.ac.kr>
2022-03-14 00:05:47 +0900

    Fix: 로그인 상태에 따른 navbar관리

--- src/components/NavBar/MyMenu/MyNetworksButton/Index.js
+++ src/components/NavBar/MyMenu/MyNetworksButton/Index.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Index = () => {
 	return (
 		<>
-			<Link to="/MyNetworks"> 내가 업로드한 모델리스트</Link>
+			<Link to="/MyNetworks">업로드한 모델</Link>
 		</>
 	);
 };
