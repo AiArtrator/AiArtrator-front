b8a96a8087b807d20501c7a8a778e2a32adff116
DODO <thsehgml37@korea.ac.kr>
2022-05-02 23:44:02 +0900

    Fix: navbar token balance

--- src/components/NavBar/Index.js
+++ src/components/NavBar/Index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './navbar.scss';
 import ModelLists from './ModelLists/Index.js';
 import MyMenus from './MyMenu/Index.js';
@@ -10,10 +10,27 @@ import OwnNetworksBtn from './MyMenu/MyNetworksButton/Index.js';
 import { useSelector } from 'react-redux';
 import MainLogo from '../../assets/logo/MainLogoH1.png';
 import { useNavigate } from 'react-router-dom';
+import { tokenStatusInNav } from '../../axios/User';
 
 const Index = () => {
 	const accesstoken = useSelector((state) => state.user.accesstoken);
 	const navigate = useNavigate();
+	const [nowToken, setNowToken] = useState('0');
+
+	useEffect(() => {
+		const fetchData = async () => {
+			setNowToken(0);
+			try {
+				const response = await tokenStatusInNav(accesstoken); // TODO : replace to getNetworkByUserId
+				setNowToken(response.data.data.balance);
+				console.log(response.data);
+				console.log(response.data.data.balance);
+			} catch (err) {
+				console.error(err);
+			}
+		};
+		fetchData();
+	}, []);
 	return (
 		<div className="nav-layout">
 			<img
@@ -44,15 +61,33 @@ const Index = () => {
 								</li>
 								<li
 									style={{
-										height: '5rem',
+										height: '0.6rem',
+										fontSize: '0.5rem',
 										color: 'white',
 										background: 'rgba(0, 0, 0, 0.728)',
 										verticalAlign: 'center',
+										display: 'block',
+										lineHeight: '30px',
+										margin: '0px',
+										padding: '10px',
 									}}
 								>
 									보유 토큰
-									<br />
-									000 Token
+								</li>
+								<li
+									style={{
+										height: '2rem',
+										fontWeight: '400',
+										color: 'white',
+										background: 'rgba(0, 0, 0, 0.728)',
+										verticalAlign: 'center',
+										display: 'block',
+
+										margin: '0px',
+										padding: '0px 10px',
+									}}
+								>
+									{nowToken} Token
 								</li>
 							</ul>
 						</li>
