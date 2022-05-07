7f8b9f36b5f5fe09ae69b7e54b6dc55b7f5667bb
DODO <thsehgml37@korea.ac.kr>
2022-05-07 22:50:41 +0900

    Feat: my balance page

--- src/page/MyBalancePage/Index.js
+++ src/page/MyBalancePage/Index.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Index = () => {
+	const usernickname = useSelector((state) => state.user?.user?.nickname);
+	// const [nowBalance, setNowBalance] = useState(null);
 	console.log('내 토큰 내역 및 충전 페이지');
 	return (
-		<>
+		<div className="my-balance-form">
+			<br />
 			<h1>나의 토큰 내역 페이지 입니다</h1>
-		</>
+			{usernickname} 님의 현재 보유 토큰
+			{/* {nowBalance} Token */}
+			<div>충전하기</div>
+			<div>*1토큰은 10원입니다.</div>
+			<div className="my-history">
+				<h4>토큰 내역 조회</h4>
+			</div>
+		</div>
 	);
 };
 
