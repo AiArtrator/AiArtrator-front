ed0e86ab563e05c1805430e736c9b986372f0e20
junhyeog <junhyeog@.korea.ac.kr>
2022-09-08 22:32:11 +0900

    fix: only one inference

--- src/components/NetworkDetail/networkDetail.js
+++ src/components/NetworkDetail/networkDetail.js
@@ -145,7 +145,7 @@ const Index = () => {
 			const res = await postInference(
 				accesstoken,
 				detailInfo.weightUuid,
-				numImg || 0,
+				1, // ! only one // numImg || 0,
 				-detailInfo.fee
 			);
 			console.log(res);
@@ -222,7 +222,9 @@ const Index = () => {
 					<input
 						className="numImg"
 						type="number"
-						value={numImg}
+						// value={numImg} // ! only one
+						value="1" // ! only one
+						disabled // ! only one
 						onChange={handleNumImg}
 						min="0"
 						max="99999" // TODO: fix max value
